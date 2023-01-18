0e75df33272389cca3142efa04ca71166d224510
sushthecoda <coolsushaan@gmail.com>
2023-01-18 12:06:43 +0530

    q1

---
+++ hello_world.go
@@ -0,0 +1,15 @@
+// package main
+
+// import "fmt"
+// // HelloWorld greets the world.
+
+// func main() {
+// 	fmt.Println("Hello, World!");
+// }
+package greeting
+
+// HelloWorld greets the world.
+func HelloWorld() string {
+	return "Hello, World!"
+}
+
